0f74ff3fb88f320ddafaf289d337d0d91f1e897c
Sergio-Estrada <pabsindustrialtecate@gmail.com>
2026-08-18 15:58:50 -0700

    Create HomePage component in script.js
    Add a basic HomePage component with a welcome message.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+// En React/Next.js:
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Bienvenido a nuestro sitio</h1>
+      {/* Elimina o comenta la línea del componente CURP */}
+      {/* <CurpGenerator /> */}
+    </main>
+  );
+}
