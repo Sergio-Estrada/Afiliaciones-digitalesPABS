5b765b774212e62c9e8ef8ec707a03c07f4a619d
Sergio-Estrada <pabsindustrialtecate@gmail.com>
2026-08-18 17:04:00 -0700

    Add form data submission to Google Apps Script

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const formData = {
+  nombre: document.getElementById('nombre').value,
+  telefono: document.getElementById('telefono').value,
+  correo: document.getElementById('correo').value
+};
 
+fetch('https://script.google.com/macros/s/AKfycbwwavpbXN_B7WeL8sScINI9ctXYnYtrw8NsntDYDXhAEuTDb1QHmCjwb-dkywNsUn86Qg/exec', {
+  method: 'POST',
+  mode: 'no-cors',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(formData)
+})
+.then(response => alert('Registro completado con éxito'))
+.catch(error => console.error('Error:', error));
